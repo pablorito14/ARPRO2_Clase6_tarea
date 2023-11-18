a6a9785b165adf72628da158f19ed412e8fa6d21
Pablo Rito <pablorito14@gmail.com>
2023-11-18 13:17:25 -0300

    completado

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -1,22 +1,69 @@
-import { Box, Button, Card, CardBody, Checkbox, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Icon, Input, InputGroup, InputRightElement, Link } from "@chakra-ui/react";
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, Card, CardBody, Checkbox, CloseButton, Flex, FormControl, FormErrorMessage,FormLabel, Heading, Icon, Input, InputGroup, InputRightElement, Link, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure, useToast } from "@chakra-ui/react";
 import {motion} from 'framer-motion';
 import {Formik} from 'formik';
 import { ref, object, string, boolean} from 'yup';
 import { useState } from "react";
-import { FaRegEye } from 'react-icons/fa6';
 import { IoEye,IoEyeOff  } from "react-icons/io5";
 
+const Lorem = ({count}) => {
+  return (
+  <>
+     {[...Array(count)].map((c,i) => 
+     <Box mt={4}>
+      <Heading size='sm'>{i+1} - Lorem ipsum dolor sit amet</Heading>
+      <Text fontSize='sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In minima tenetur ex facere numquam vel. Odio hic molestiae aperiam expedita, dolorem perferendis, quo non facilis officiis optio, accusamus laborum quibusdam?</Text>
+     </Box>)}
+      
+  </>
+  )
+}
+const TerminosModal = ({isOpen,onClose}) => {
 
-// valores de los input del formulario
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false} scrollBehavior='inside'>
+      <ModalOverlay />
+        <ModalContent>
+          <ModalHeader pb={0}>Términos y condiciones</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={6} />
+          </ModalBody>
+
+          <ModalFooter>
+          <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.9}}>
+              <Button bg='#112132' color='#aaccee' onClick={onClose}
+                      _hover={{ bg:'#aaccee', color:'#112132'}}
+                      >Cerrar</Button>
+
+            </motion.div>
+          </ModalFooter>
+        </ModalContent>
+    </Modal>
+  )
+}
+
 const Formulario = () => {
+
+  const toast = useToast();
 
   const [show,setShow] = useState(false);
   const mostrarPass = () => setShow(!show);
 
   const [showConfirm,setShowConfirm] = useState(false);
   const mostrarConfirmPass = () => setShowConfirm(!showConfirm);
+  
+  // valores iniciales de los input del formulario
+  // const formInitialValues = {
+  //   nombre:'pablo',
+  //   apellido:'ritp',
+  //   email:'pablo@pablo.com',
+  //   tel:'3517543211',
+  //   pass:'12345678',
+  //   confPass:'12345678',
+  //   toc:true
+  // }
 
-  const formValues = {
+  const formInitialValues = {
     nombre:'',
     apellido:'',
     email:'',
@@ -39,12 +86,45 @@ const Formulario = () => {
   }
   
   // funcion a ejecutar cuando se envia el formulario
-  const formSubmit = (values, {setSubmitting}) => {
+  const {isOpen,onOpen,onClose} = useDisclosure();
+
+  const formSubmit = (values, {setSubmitting,resetForm}) => {
     setTimeout(() => {
       console.log(values);
-      // alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 1400);
+      
+      toast({
+        position: 'top',
+        duration: null,
+        render: () => (
+          <Alert
+            status='success'
+            variant='subtle'
+            flexDirection='column'
+            alignItems='center'
+            justifyContent='center'
+            textAlign='center'
+            rounded='md'
+            
+          >
+            <AlertIcon boxSize='40px' mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize='lg'>
+              Registro completado
+            </AlertTitle>
+            <AlertDescription maxWidth='md'>
+              <Text as='strong'>{values.nombre}</Text> le hemos enviado un email a <Text as='strong'>{values.email}</Text> para confirmar su cuenta. Recuerde revisar la carpeta de SPAM o Correo no deseado en caso de no recibirlo.
+            </AlertDescription>
+            <Flex mt={2}>
+              <Button colorScheme="green" onClick={() => {
+                  setSubmitting(false);
+                  toast.closeAll();
+                  resetForm({values:formInitialValues})
+                }}>Aceptar</Button>
+
+            </Flex>
+          </Alert>
+        ),
+      })
+    }, 400);
   }
 
   // validacion con YUP mediante schemas
@@ -64,7 +144,9 @@ const Formulario = () => {
       <CardBody>
         <Heading textAlign='center' as='h3' mb={3} size='lg'>Formulario de registro</Heading>
 
-        <Formik initialValues={formValues}
+        
+
+        <Formik initialValues={formInitialValues}
                 // validate={formValidate}
                 validationSchema={registroSchema}
                 onSubmit={formSubmit}>
@@ -79,7 +161,7 @@ const Formulario = () => {
                         focusBorderColor='blue.200'
                         placeholder="Ingrese su nombre"
                         onChange={handleChange} onBlur={handleBlur} 
-                        value={values.nombre} />
+                        value={values.nombre} isDisabled={isSubmitting} />
                         <FormErrorMessage mt={0} position='absolute'>{errors.nombre}</FormErrorMessage>
               </FormControl>
 
@@ -90,7 +172,7 @@ const Formulario = () => {
                         focusBorderColor='blue.200'
                         placeholder="Ingrese su apellido"
                         onChange={handleChange} onBlur={handleBlur} 
-                        value={values.apellido}/>
+                        value={values.apellido} isDisabled={isSubmitting}/>
                 <FormErrorMessage mt={0} position='absolute'>{errors.apellido}</FormErrorMessage>
               </FormControl>
 
@@ -101,7 +183,7 @@ const Formulario = () => {
                         focusBorderColor='blue.200'
                         placeholder="Ingrese su correo"
                         onChange={handleChange} onBlur={handleBlur} 
-                        value={values.email} />
+                        value={values.email} isDisabled={isSubmitting}/>
                 <FormErrorMessage mt={0} position='absolute'>{errors.email}</FormErrorMessage>
               </FormControl>
 
@@ -112,7 +194,7 @@ const Formulario = () => {
                         focusBorderColor='blue.200'
                         placeholder="Teléfono"
                         onChange={handleChange} onBlur={handleBlur} 
-                        value={values.tel}/>
+                        value={values.tel} isDisabled={isSubmitting}/>
                 <FormErrorMessage mt={0} position='absolute'>{errors.tel}</FormErrorMessage>
               </FormControl>
 
@@ -124,9 +206,9 @@ const Formulario = () => {
                         focusBorderColor='blue.200'
                         placeholder="Ingrese una contraseña"
                         onChange={handleChange} onBlur={handleBlur} 
-                        value={values.pass}/>
+                        value={values.pass} isDisabled={isSubmitting}/>
                   <InputRightElement>
-                    <Button onClick={mostrarPass} bg='transparent' _hover={{ bg:'transparent' }} tabIndex='-1'>
+                    <Button onClick={mostrarPass} bg='transparent' _hover={{ bg:'transparent' }} tabIndex='-1' isDisabled={isSubmitting}>
                       <Icon boxSize={8} p={2} as={(!show) ? IoEye : IoEyeOff } />
                     </Button>
                   </InputRightElement>
@@ -144,9 +226,9 @@ const Formulario = () => {
                         focusBorderColor='blue.200'
                         placeholder="Ingrese nuevamente la contraseña"
                         onChange={handleChange} onBlur={handleBlur} 
-                        value={values.confPass}/>
+                        value={values.confPass} isDisabled={isSubmitting}/>
                   <InputRightElement>
-                    <Button onClick={mostrarConfirmPass} bg='transparent' _hover={{ bg:'transparent' }} tabIndex='-1'>
+                    <Button onClick={mostrarConfirmPass} bg='transparent' _hover={{ bg:'transparent' }} tabIndex='-1' isDisabled={isSubmitting}>
                       <Icon boxSize={8} p={2} as={(!showConfirm) ? IoEye : IoEyeOff } />
                     </Button>
                   </InputRightElement>
@@ -155,12 +237,12 @@ const Formulario = () => {
               </FormControl>
               {/* TERMINOS */}
               
-              <FormControl isInvalid={errors.toc && touched.toc} mb={6}>
-                <Checkbox name="toc"
-                        onChange={handleChange} value={values.toc}>
-                  He leido y acepto los <Link>términos y condiciones</Link>
-                </Checkbox>
-                {/* <FormErrorMessage mt={0} position='absolute'>{errors.toc}</FormErrorMessage> */}
+              <FormControl isInvalid={errors.toc && touched.toc} 
+                          display='flex' gap={3}  mb={6}>
+                <Checkbox name="toc" onChange={handleChange} value={values.toc} isChecked={values.toc} isDisabled={isSubmitting}></Checkbox>
+                <Text>
+                He leído y acepto los <Link textDecoration='none' onClick={(!isSubmitting) ? onOpen : null} color='#768798' as='a'>términos y condiciones</Link>
+                </Text>
               </FormControl>
 
               <Box display='flex' justifyContent='center'>
@@ -168,7 +250,7 @@ const Formulario = () => {
                   <Button type='submit' isLoading={isSubmitting} size='lg' 
                           bg='#112132' color='#aaccee'
                           _hover={{ bg:'#aaccee', color:'#112132'}}
-                          loadingText='Submitting'>Crear cuenta</Button>
+                          loadingText='Procesando'>Crear cuenta</Button>
 
                 </motion.div>
               </Box>
@@ -176,6 +258,9 @@ const Formulario = () => {
             </form>
           )}
         </Formik>
+
+
+        <TerminosModal isOpen={isOpen} onClose={onClose} />
       </CardBody>
     </Card>
     </>
